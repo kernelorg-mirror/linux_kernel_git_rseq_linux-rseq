bf3b2975629af5e014af3ee2fb73e16c8f3164fe
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-05-30 09:46:53 +0200

    Restartable sequences: Provide self-tests
    Implements two basic tests of RSEQ functionality, and one more
    exhaustive parameterizable test.
    
    The first, "basic_test" only asserts that RSEQ works moderately
    correctly.
    E.g. that:
    - The CPUID pointer works
    - Code infinitely looping within a critical section will eventually be
      interrupted.
    - Critical sections are interrupted by signals.
    
    "basic_percpu_ops_test" is a slightly more "realistic" variant,
    implementing a few simple per-cpu operations and testing their
    correctness.
    
    "param_test" is a parametrizable restartable sequences test. See
    the "--help" output for usage.
    
    As part of those tests, a helper library "rseq" implements a user-space
    API around restartable sequences. It uses the cpu_opv system call as
    fallback when single-stepped by a debugger. It exposes the instruction
    pointer addresses where the rseq assembly blocks begin and end, as well
    as the associated abort instruction pointer, in the __rseq_table
    section. This section allows debuggers may know where to place
    breakpoints when single-stepping through assembly blocks which may be
    aborted at any point by the kernel.
    
    The following rseq APIs are implemented in this helper library:
    - rseq_register_current_thread()/rseq_unregister_current_thread():
        register/unregister current thread's use of rseq,
    - rseq_current_cpu_raw():
        current CPU number,
    - rseq_start():
        beginning of a restartable sequence,
    - rseq_cpu_at_start():
        CPU number at start of restartable sequence,
    - rseq_finish():
        End of restartable sequence made of zero or more loads, completed by
        a word-sized store,
    - rseq_finish2():
        End of restartable sequence made of zero or more loads, one
        speculative word-sized store, completed by a word-sized store,
    - rseq_finish2_release():
        End of restartable sequence made of zero or more loads, one
        speculative word-sized store, completed by a word-sized store with
        release semantic,
    - rseq_finish_memcpy():
        End of restartable sequence made of zero or more loads, a
        speculative copy of a variable length memory region, completed by a
        word-sized store.
    - rseq_finish_memcpy_release():
        End of restartable sequence made of zero or more loads, a
        speculative copy of a variable length memory region, completed by a
        word-sized store with release semantic.
    
    PowerPC tests have been implemented by Boqun Feng.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Russell King <linux@arm.linux.org.uk>
    CC: Catalin Marinas <catalin.marinas@arm.com>
    CC: Will Deacon <will.deacon@arm.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Paul Turner <pjt@google.com>
    CC: Andrew Hunter <ahh@google.com>
    CC: Peter Zijlstra <peterz@infradead.org>
    CC: Andy Lutomirski <luto@amacapital.net>
    CC: Andi Kleen <andi@firstfloor.org>
    CC: Dave Watson <davejwatson@fb.com>
    CC: Chris Lameter <cl@linux.com>
    CC: Ingo Molnar <mingo@redhat.com>
    CC: "H. Peter Anvin" <hpa@zytor.com>
    CC: Ben Maurer <bmaurer@fb.com>
    CC: Steven Rostedt <rostedt@goodmis.org>
    CC: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    CC: Josh Triplett <josh@joshtriplett.org>
    CC: Linus Torvalds <torvalds@linux-foundation.org>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: Boqun Feng <boqun.feng@gmail.com>
    CC: Shuah Khan <shuah@kernel.org>
    CC: linux-kselftest@vger.kernel.org
    CC: linux-api@vger.kernel.org

---
+++ tools/testing/selftests/rseq/basic_test.c
@@ -0,0 +1,97 @@
+/*
+ * Basic test coverage for critical regions and rseq_current_cpu().
+ */
+
+#define _GNU_SOURCE
+#include <assert.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include "rseq.h"
+
+volatile int signals_delivered;
+volatile __thread struct rseq_state sigtest_start;
+
+void test_cpu_pointer(void)
+{
+	cpu_set_t affinity, test_affinity;
+	int i;
+
+	sched_getaffinity(0, sizeof(affinity), &affinity);
+	CPU_ZERO(&test_affinity);
+	for (i = 0; i < CPU_SETSIZE; i++) {
+		if (CPU_ISSET(i, &affinity)) {
+			CPU_SET(i, &test_affinity);
+			sched_setaffinity(0, sizeof(test_affinity),
+					&test_affinity);
+			assert(rseq_current_cpu() == sched_getcpu());
+			assert(rseq_current_cpu() == i);
+			CPU_CLR(i, &test_affinity);
+		}
+	}
+	sched_setaffinity(0, sizeof(affinity), &affinity);
+}
+
+/*
+ * This depends solely on some environmental event triggering a counter
+ * increase.
+ */
+void test_critical_section(void)
+{
+	struct rseq_state start;
+	uint32_t event_counter;
+
+	start = rseq_start();
+	event_counter = start.event_counter;
+	do {
+		start = rseq_start();
+	} while (start.event_counter == event_counter);
+}
+
+void test_signal_interrupt_handler(int signo)
+{
+	struct rseq_state current;
+
+	current = rseq_start();
+	/*
+	 * The potential critical section bordered by 'start' must be
+	 * invalid.
+	 */
+	assert(current.event_counter != sigtest_start.event_counter);
+	signals_delivered++;
+}
+
+void test_signal_interrupts(void)
+{
+	struct itimerval it = { { 0, 1 }, { 0, 1 } };
+	struct itimerval stop_it = { { 0, 0 }, { 0, 0 } };
+
+	setitimer(ITIMER_PROF, &it, NULL);
+	signal(SIGPROF, test_signal_interrupt_handler);
+
+	do {
+		sigtest_start = rseq_start();
+	} while (signals_delivered < 10);
+	setitimer(ITIMER_PROF, &stop_it, NULL);
+}
+
+int main(int argc, char **argv)
+{
+	if (rseq_register_current_thread())
+		goto init_thread_error;
+	printf("testing current cpu\n");
+	test_cpu_pointer();
+	printf("testing critical section\n");
+	test_critical_section();
+	printf("testing critical section is interrupted by signal\n");
+	test_signal_interrupts();
+	if (rseq_unregister_current_thread())
+		goto init_thread_error;
+	return 0;
+
+init_thread_error:
+	return -1;
+}
