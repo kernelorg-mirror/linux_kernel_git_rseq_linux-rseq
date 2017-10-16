11e0528926fd7fe02039dae54479a60dfac466e6
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2017-10-16 15:03:19 -0400

    Restartable sequences selftests: Update following rseq ABI improvement
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Russell King <linux@arm.linux.org.uk>
    CC: Catalin Marinas <catalin.marinas@arm.com>
    CC: Will Deacon <will.deacon@arm.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Paul Turner <pjt@google.com>
    CC: Andrew Hunter <ahh@google.com>
    CC: Peter Zijlstra <peterz@infradead.org>
    CC: Andy Lutomirski <luto@amacapital.net>
    CC: Andi Kleen <andi@firstfloor.org>
    CC: Dave Watson <davejwatson@fb.com>
    CC: Chris Lameter <cl@linux.com>
    CC: Ingo Molnar <mingo@redhat.com>
    CC: "H. Peter Anvin" <hpa@zytor.com>
    CC: Ben Maurer <bmaurer@fb.com>
    CC: Steven Rostedt <rostedt@goodmis.org>
    CC: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    CC: Josh Triplett <josh@joshtriplett.org>
    CC: Linus Torvalds <torvalds@linux-foundation.org>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: Boqun Feng <boqun.feng@gmail.com>
    CC: Shuah Khan <shuah@kernel.org>
    CC: linux-kselftest@vger.kernel.org
    CC: linux-api@vger.kernel.org

--- tools/testing/selftests/rseq/rseq.c
+++ tools/testing/selftests/rseq/rseq.c
@@ -32,7 +32,6 @@
 __attribute__((weak)) __thread volatile struct rseq __rseq_abi = {
 	.u.e.cpu_id = -1,
 };
-__attribute__((weak)) uint32_t rseq_sig = RSEQ_SIG;
 
 static int sys_rseq(volatile struct rseq *rseq_abi, int flags, uint32_t sig)
 {
@@ -43,7 +42,7 @@ int rseq_register_current_thread(void)
 {
 	int rc;
 
-	rc = sys_rseq(&__rseq_abi, 0, rseq_sig);
+	rc = sys_rseq(&__rseq_abi, 0, RSEQ_SIG);
 	if (rc) {
 		fprintf(stderr, "Error: sys_rseq(...) failed(%d): %s\n",
 			errno, strerror(errno));
@@ -57,7 +56,7 @@ int rseq_unregister_current_thread(void)
 {
 	int rc;
 
-	rc = sys_rseq(NULL, 0, 0);
+	rc = sys_rseq(&__rseq_abi, RSEQ_FLAG_UNREGISTER, RSEQ_SIG);
 	if (rc) {
 		fprintf(stderr, "Error: sys_rseq(...) failed(%d): %s\n",
 			errno, strerror(errno));
