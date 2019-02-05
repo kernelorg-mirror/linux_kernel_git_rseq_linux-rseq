08474f96331a00c7faf45b5ec8cfc7d8eef30be3
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2019-02-05 15:33:54 -0500

    rseq: Introduce RSEQ_FLAG_NODE_ID
    glibc 2.29 introduced an API for getcpu(2), which returns both the
    current cpu number and the current NUMA node id.
    
    Considering that the mapping between CPU number and node id can change
    during the lifetime of a process (e.g. CRIU), the current node id
    should be kept in a TLS variable to accelerate glibc getcpu().
    
    Quickly accessing the current NUMA node ID is useful to improve glibc
    NUMA-awareness of locking implementation.
    
    Since struct rseq cannot be extended (it is fixed-size on purpose to
    allow being defined by many libraries/application within the same
    process), we need to register a separate TLS variable that holds the
    current NUMA node id.
    
    Link: https://public-inbox.org/libc-alpha/871s6p7es1.fsf@oldenburg2.str.redhat.com/
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Carlos O'Donell <carlos@redhat.com>
    CC: Florian Weimer <fweimer@redhat.com>
    CC: Joseph Myers <joseph@codesourcery.com>
    CC: Szabolcs Nagy <szabolcs.nagy@arm.com>
    CC: H.J. Lu <hjl.tools@gmail.com>
    CC: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    CC: Peter Zijlstra <peterz@infradead.org>
    CC: Paul Turner <pjt@google.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Andy Lutomirski <luto@amacapital.net>
    CC: Andi Kleen <andi@firstfloor.org>
    CC: Dave Watson <davejwatson@fb.com>
    CC: Chris Lameter <cl@linux.com>
    CC: Ingo Molnar <mingo@redhat.com>
    CC: "H. Peter Anvin" <hpa@zytor.com>
    CC: Ben Maurer <bmaurer@fb.com>
    CC: Steven Rostedt <rostedt@goodmis.org>
    CC: Josh Triplett <josh@joshtriplett.org>
    CC: Linus Torvalds <torvalds@linux-foundation.org>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: Russell King <linux@arm.linux.org.uk>
    CC: Catalin Marinas <catalin.marinas@arm.com>
    CC: Will Deacon <will.deacon@arm.com>
    CC: Michael Kerrisk <mtk.manpages@gmail.com>
    CC: Boqun Feng <boqun.feng@gmail.com>
    CC: linux-api@vger.kernel.org

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1060,6 +1060,7 @@ struct task_struct {
 #ifdef CONFIG_RSEQ
 	struct rseq __user *rseq;
 	u32 rseq_sig;
+	u32 __user *rseq_node_id;
 	/*
 	 * RmW on rseq_event_mask must be performed atomically
 	 * with respect to preemption.
@@ -1852,10 +1853,12 @@ static inline void rseq_fork(struct task_struct *t, unsigned long clone_flags)
 	if (clone_flags & CLONE_THREAD) {
 		t->rseq = NULL;
 		t->rseq_sig = 0;
+		t->rseq_node_id = NULL;
 		t->rseq_event_mask = 0;
 	} else {
 		t->rseq = current->rseq;
 		t->rseq_sig = current->rseq_sig;
+		t->rseq_node_id = current->rseq_node_id;
 		t->rseq_event_mask = current->rseq_event_mask;
 	}
 }
@@ -1864,6 +1867,7 @@ static inline void rseq_execve(struct task_struct *t)
 {
 	t->rseq = NULL;
 	t->rseq_sig = 0;
+	t->rseq_node_id = NULL;
 	t->rseq_event_mask = 0;
 }
 
