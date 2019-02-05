451e67444e191b38ffba194af8b80d00ba4b2e1f
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2019-02-05 15:33:54 -0500

    rseq: Introduce RSEQ_FLAG_NODE_ID
    glibc 2.29 introduced an API for getcpu(2), which returns both the
    current cpu number and the current NUMA node id.
    
    Considering that the mapping between CPU number and node id can change
    during the lifetime of a process (e.g. CRIU), the current node id
    should be kept in a TLS variable to accelerate glibc getcpu().
    
    Quickly accessing the current NUMA node ID is useful to improve glibc
    NUMA-awareness of locking implementation.
    
    Since struct rseq cannot be extended (it is fixed-size on purpose to
    allow being defined by many libraries/application within the same
    process), we need to register a separate TLS variable that holds the
    current NUMA node id.
    
    Link: https://public-inbox.org/libc-alpha/871s6p7es1.fsf@oldenburg2.str.redhat.com/
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Carlos O'Donell <carlos@redhat.com>
    CC: Florian Weimer <fweimer@redhat.com>
    CC: Joseph Myers <joseph@codesourcery.com>
    CC: Szabolcs Nagy <szabolcs.nagy@arm.com>
    CC: H.J. Lu <hjl.tools@gmail.com>
    CC: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    CC: Peter Zijlstra <peterz@infradead.org>
    CC: Paul Turner <pjt@google.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Andy Lutomirski <luto@amacapital.net>
    CC: Andi Kleen <andi@firstfloor.org>
    CC: Dave Watson <davejwatson@fb.com>
    CC: Chris Lameter <cl@linux.com>
    CC: Ingo Molnar <mingo@redhat.com>
    CC: "H. Peter Anvin" <hpa@zytor.com>
    CC: Ben Maurer <bmaurer@fb.com>
    CC: Steven Rostedt <rostedt@goodmis.org>
    CC: Josh Triplett <josh@joshtriplett.org>
    CC: Linus Torvalds <torvalds@linux-foundation.org>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: Russell King <linux@arm.linux.org.uk>
    CC: Catalin Marinas <catalin.marinas@arm.com>
    CC: Will Deacon <will.deacon@arm.com>
    CC: Michael Kerrisk <mtk.manpages@gmail.com>
    CC: Boqun Feng <boqun.feng@gmail.com>
    CC: linux-api@vger.kernel.org

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -67,7 +67,6 @@ struct old_linux_dirent;
 struct perf_event_attr;
 struct file_handle;
 struct sigaltstack;
-struct rseq;
 struct cpu_op;
 union bpf_attr;
 
@@ -925,7 +924,7 @@ asmlinkage long sys_pkey_alloc(unsigned long flags, unsigned long init_val);
 asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
-asmlinkage long sys_rseq(struct rseq __user *rseq, uint32_t rseq_len,
+asmlinkage long sys_rseq(void __user *uptr, uint32_t len,
 			 int flags, uint32_t sig);
 asmlinkage long sys_cpu_opv(struct cpu_op __user *ucpuopv, int cpuopcnt,
 			int cpu, int flags);
